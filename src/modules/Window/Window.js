4f40fcd97bfa4f1081d238fc17ebdbfbb13ff40e
fabianrmz <a01630024@itesm.mx>
2021-03-30 14:30:39 -0600

    feat: window content added and beatifully grown

--- src/modules/Window/Window.js
+++ src/modules/Window/Window.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Style from './Window.module.css';
+import userIcon from './userIcon.png';
 
 const Window = () =>{
 
     return (
-        <>
-            Window
-        </>
+        <div className={Style.windowContainer}>
+            <div className={Style.topWindow}>
+                <div className={Style.captionWindow}>
+                    <img alt={"img"} src={userIcon}/>
+                    <p>Welcome to my profile!</p>
+                </div>
+            
+                <div className={Style.closeX}>
+                        <p>X</p>
+                </div>
+                <div className={Style.windowBlueOption}>
+                        <p>□</p>
+                </div>
+                <div className={Style.windowBlueOption}>
+                        <p>_</p>
+                </div>
+                <div className={Style.windowContent}>
+                    This page is in working progress
+                </div>
+            </div>
+            
+            <div>
+
+            </div>
+        
+        </div>
     )
 }
 export default Window;
